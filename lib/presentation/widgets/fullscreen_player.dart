b6c6994f8be6a290fc0d0eb22c0b6f09af9e7745
Laura Avila <lavila@bigwise.com>
2025-02-10 17:03:33 -0400

    FEAT: Video Caption

--- lib/presentation/widgets/fullscreen_player.dart
+++ lib/presentation/widgets/fullscreen_player.dart
@@ -5,7 +5,8 @@ class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
-  const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
+  const FullScreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
 
   @override
   State<FullScreenPlayer> createState() => _FullScreenPlayerState();
@@ -18,9 +19,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
   void initState() {
     super.initState();
     controller = VideoPlayerController.asset(widget.videoUrl)
-    ..setVolume(0)
-    ..setLooping(true)
-    ..play();
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
   }
 
   @override
@@ -31,10 +32,49 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: controller.initialize(), 
-    builder: (context, snapshot) {
-      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
-    }
+    return FutureBuilder(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+                child: CircularProgressIndicator(strokeWidth: 2));
+          }
+          return AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(children: [
+              //Video
+              VideoPlayer(controller),
+              //gradient
+
+              //caption
+              Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: _VideoCaption(caption: widget.caption))
+            ]),
+          );
+        });
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  const _VideoCaption({
+    required this.caption,
+  });
+
+  final String caption;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final tileStyle = Theme.of(context).textTheme.titleLarge!;
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: tileStyle,
+      )
     );
   }
 }
