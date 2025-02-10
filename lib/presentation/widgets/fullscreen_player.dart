38634e930b3a8615149c61855826cf8663dc6792
Laura Avila <lavila@bigwise.com>
2025-02-10 17:06:30 -0400

    FEAT: pause video

--- lib/presentation/widgets/fullscreen_player.dart
+++ lib/presentation/widgets/fullscreen_player.dart
@@ -39,19 +39,28 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             return const Center(
                 child: CircularProgressIndicator(strokeWidth: 2));
           }
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(children: [
-              //Video
-              VideoPlayer(controller),
-              //gradient
-
-              //caption
-              Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: _VideoCaption(caption: widget.caption))
-            ]),
+          return GestureDetector(
+            onTap: (){
+              if(controller.value.isPlaying){
+                controller.pause();
+                return;
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(children: [
+                //Video
+                VideoPlayer(controller),
+                //gradient
+            
+                //caption
+                Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: _VideoCaption(caption: widget.caption))
+              ]),
+            ),
           );
         });
   }
