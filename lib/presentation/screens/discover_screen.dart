55a08b9f4c6512cdd1e9fa06f3f462f4f97e5447
Laura Avila <lavila@bigwise.com>
2025-02-10 14:12:01 -0400

    FEAT: CircularProgressIndicator when loading videos and Pageview to scroll videos

--- lib/presentation/screens/discover_screen.dart
+++ lib/presentation/screens/discover_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tok_tik_app/presentation/provider/discover_provider.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final discoverProvider = context.watch<DiscoverProvider>();
+
     return Scaffold(
-      body: Center(child: Text("Discover Screen")),
+      body: discoverProvider.initialLoading ? 
+      const Center(child: CircularProgressIndicator(strokeWidth: 2),) : 
+      const Placeholder(),
     );
   }
 }
---
+++ lib/presentation/widgets/video_scrollable_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tok_tik_app/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      children: [],
+    );
+  }
+}
