55bd18022c1f3ee34cef681a174c179a8c38e8af
Laura Avila <lavila@bigwise.com>
2025-02-10 14:53:59 -0400

    FEAT: like and views button

--- lib/presentation/widgets/video_scrollable_view.dart
+++ lib/presentation/widgets/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tok_tik_app/domain/entities/video_post.dart';
+import 'package:tok_tik_app/presentation/widgets/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,10 +9,23 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: BouncingScrollPhysics(),
-      children: [],
+      itemCount: videos.length,
+      itemBuilder: (_, index) { 
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children:[
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost))
+          ]
+        );
+
+      }
     );
   }
 }
+
