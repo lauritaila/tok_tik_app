7ff7c21764858a1183f188067f65ef19d7c5e762
Laura Avila <lavila@bigwise.com>
2025-02-10 14:03:01 -0400

    FEAT: read and assign videos to provider

--- lib/presentation/provider/discover_provider.dart
+++ lib/presentation/provider/discover_provider.dart
@@ -1,13 +1,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:tok_tik_app/domain/entities/video_post.dart';
+import 'package:tok_tik_app/infrastructure/models/local_video_model.dart';
+import 'package:tok_tik_app/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository, DataSource
+
   bool initialLoading = true;
   final List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    //Todo: donload videos
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts.map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity()).toList();
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
   
