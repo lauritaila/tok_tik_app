698df89d0cf190a88171abe12dc880a42b6a5bcf
Laura Avila <lavila@bigwise.com>
2025-02-11 10:00:53 -0400

    FEAT: implement datasources and repositories (clean architecture)

---
+++ lib/domain/repositories/video_posts_repository.dart
@@ -0,0 +1,6 @@
+import 'package:tok_tik_app/domain/entities/video_post.dart';
+
+abstract class VideoPostsRepository{
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID);
+}
